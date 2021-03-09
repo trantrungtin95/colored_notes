e5fb2f70f729a2db411e55d930749325ebc09aae
tin tran <tintran@Macbooks-MacBook-Pro.local>
2021-03-09 18:42:45 +0700

    Commit config mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,7 +110,7 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.action_mailer.delivery_method = :smtp
-  host = 'trantrungtinbkit@gmail.com' #replace with your own url
+  host = 'https://colored-notes.herokuapp.com/' #replace with your own url
   config.action_mailer.default_url_options = { host: host }
 
   # SMTP settings for gmail
